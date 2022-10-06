e60e175d07e288344be4f83816271a38686508b3
vinodkumarch <46564790+vinodkumarch@users.noreply.github.com>
2022-10-06 19:17:47 +0530

    added header and sub header changes

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h2>Hello {{name}}!</h2>`,
+  template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`],
 })
 export class HelloComponent {
